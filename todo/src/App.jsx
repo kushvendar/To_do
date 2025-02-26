f07f14f57cfe26331913f52c3ba6787f6785a84f
Kush <jangidkush1234@gmail.com>
2025-02-26 23:27:51 +0530

    frontend mostly done

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import checked from './images/checked.png'
+import icon from './images/icon.png'
+import unchecked from './images/unchecked.png'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='box-border'>
+      <div className='w-full min-h-screen p-[10px] bg-linear-135 from-[#153677] to-[#4e085f]'>
+          <div className='w-full max-w-[540px] bg-white mt-[100px] mb-[20px] ml-auto mr-auto pt-[40px] pb-[70px] pr-[30px] pl-[30px] rounded-[10px]'>
+          <h2 className='text-[#002765] flex items-center mb-[20px] font-extrabold'>
+            To-Do List
+             <img className='w-[30px] ml-[10px]' src={icon} alt="icon" />
+             </h2>
+             <div className='flex items-center justify-between bg-[#edeef0] rounded-[30px] pl-[20px] mb-[25px]'><input className='flex-1 p-[10px] bg-transparent' type="text" placeholder='Add your To-do'/>
+             <button className='p-[15px] pl-[50px] pr-[50px] bg-[#ff5945] text-white font-[16px] cursor-pointer rounded-[40px]'>Add</button></div>
+          </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+      )
 }
 
 export default App
