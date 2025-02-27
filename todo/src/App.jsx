e93fa7491ef50acf50a2d7580ad4e5118d0676a9
Kush <jangidkush1234@gmail.com>
2025-02-27 23:34:49 +0530

    frontend completed

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -5,6 +5,9 @@ import unchecked from './images/unchecked.png'
 import './index.css'
 
 function App() {
+  const inputBox= document.getElementById('input-box')
+  const listContainer=document.getElementById('list-container')
+
   return (
     <div className='box-border'>
       <div className='w-full min-h-screen p-[10px] bg-linear-135 from-[#153677] to-[#4e085f]'>
@@ -14,13 +17,12 @@ function App() {
              <img className='w-[30px] ml-[10px]' src={icon} alt="icon" />
              </h2>
              <div className='flex items-center justify-between bg-[#edeef0] rounded-[30px] pl-[20px] mb-[25px]'>
-              <input className='flex-1 p-[10px] bg-transparent' type="text" placeholder='Add your To-do'/>
+              <input className='flex-1 p-[10px] bg-transparent' id='input-box' type="text" placeholder='Add your To-do'/>
              <button className='p-[15px] pl-[50px] pr-[50px] bg-[#ff5945] text-white font-[16px] cursor-pointer rounded-[40px]'>Add</button>
              </div>
-             <ul>
-              <li className="text-[17px] pt-[12px] pl-[50px] pb-[12px] pr-[8px] cursor-pointer relative list-nonebefore:content-[''] before:bg-[url(./images/unchecked.png)] before:absolute before:h-[28px] before:w-[28px] before:rounded-[50%]  before:bg-cover before:bg-center before:top-[12px] before:left-[8px]">Task 1</li>
-              <li className="text-[17px] pt-[12px] pl-[50px] pb-[12px] pr-[8px] cursor-pointer relative list-nonebefore:content-[''] before:bg-[url(./images/unchecked.png)] before:absolute before:h-[28px] before:w-[28px] before:rounded-[50%]  before:bg-cover before:bg-center before:top-[12px] before:left-[8px]">Task 2</li>
-              <li className="text-[17px] pt-[12px] pl-[50px] pb-[12px] pr-[8px] cursor-pointer relative list-nonebefore:content-[''] before:bg-[url(./images/unchecked.png)] before:absolute before:h-[28px] before:w-[28px] before:rounded-[50%]  before:bg-cover before:bg-center before:top-[12px] before:left-[8px]">Task 3</li>
+             <ul className='id=list-container'>
+              <li  className="text-[17px] pt-[12px] pl-[50px] pb-[12px] pr-[8px] cursor-pointer relative list-none before:content-[''] before:bg-[url(./images/checked.png)] before:absolute before:h-[28px] before:w-[28px] before:rounded-[50%]  before:bg-cover before:bg-center before:top-[12px] before:left-[8px] text-[#555] line-through">Task 1</li>
+              
              </ul>
           </div>
         </div>
