73ce7b1a4fe29ae9645df17f521e285ad2549fe9
Kush <jangidkush1234@gmail.com>
2025-03-02 09:37:56 +0530

    added favicon

--- todo/src/List.jsx
+++ todo/src/List.jsx
@@ -5,7 +5,7 @@ import icon from './images/icon.png'
 import unchecked from './images/unchecked.png'
 
 function List(props) {
-  return <li  className="text-[17px] pt-[12px] pl-[50px] pb-[12px] pr-[8px] cursor-pointer relative list-none before:content-[''] before:bg-[url(./images/unchecked.png)] before:absolute before:h-[28px] before:w-[28px] before:rounded-[50%]  before:bg-cover before:bg-center before:top-[12px] before:left-[8px] text-[#555]"> {props.item}
+  return <li  className="text-[17px] pt-[12px] pl-[50px] pb-[12px] pr-[8px] cursor-pointer relative list-none before:content-[''] before:bg-[url(./images/unchecked.png)] before:absolute before:h-[28px] before:w-[28px] before:rounded-[50%]  before:bg-cover before:bg-center before:top-[12px] before:left-[8px] text-[#555]" on> {props.item}
   <span className='absolute right top-[5px] w-[40px] h-[40px] text-[22px] text-white leading-[40px] text-center rounded-[50%] hover:bg-[#edeef0 ] ' onClick={e=>{
     props.delList(props.index)
   }}>
